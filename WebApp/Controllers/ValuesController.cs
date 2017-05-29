639244a5a32b91b79808a96f5300cb356aa77499
Paul Fryer <paulfryer@macrowebservices.com>
2017-05-29 08:51:16 -0700

    moving source code

---
+++ WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("proxy")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("{*path}")]
+        [HttpPost("{*path}")]
+        [HttpPut("{*path}")]
+        [HttpOptions("{*path}")]
+        [HttpHead("{*path}")]
+        [HttpPatch("{*path}")]
+        [HttpDelete("{*path}")]
+        public string Execute(string path)
+        {
+
+            return path;
+        }
+    }
+}
---
+++ WebApp/LambdaEntryPoint.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Amazon.Lambda.AspNetCoreServer;
+
+namespace WebApplication1
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
