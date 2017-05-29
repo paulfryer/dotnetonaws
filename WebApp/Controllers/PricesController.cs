f3121730835fb9de7049a890e35730255734211a
Paul Fryer <paulfryer@macrowebservices.com>
2017-05-29 09:47:14 -0700

    starting to work on prices for spot.

---
+++ WebApp/Controllers/PricesController.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{ 
+    [Route("api/[controller]")]
+    public class PricesController : Controller {
+
+        public PricesController(  )
+        {           
+        }
+
+       [HttpGet]
+       [Route("")]
+        public async Task<IActionResult> GetAsync()
+        {
+
+            try
+            {
+                throw new NotImplementedException();
+
+            }catch(Exception ex)
+            {
+                var result = new BadRequestResult();
+                return result;
+            }            
+        }
+        
+
+    }
+}
