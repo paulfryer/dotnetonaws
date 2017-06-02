2fcfbd08b20bb165073b4ab8cf4eace71569122a
Paul Fryer <paulfryer@macrowebservices.com>
2017-06-02 06:19:38 -0700

    checking in latest.

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,11 +1,15 @@
 ﻿using Amazon.CloudWatch;
+using Amazon.CloudWatch.Model;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.EC2;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
 using Amazon.Lambda.Serialization.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,24 +29,30 @@ namespace Functions
             var controller = new SpotController();
 
 
+            var dynamoEventJson = File.OpenText("DynamoEvent.json").ReadToEnd();
+            var @event = JsonConvert.DeserializeObject<DynamoDBEvent>(dynamoEventJson);
 
+            // await controller.SyncToCloudWatch(@event, null);
 
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.APNortheast1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.APNortheast2, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.APSouth1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.APSoutheast1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.APSoutheast2, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.CACentral1, null);
-            //await controller.SyncToDynamo(Amazon.RegionEndpoint.CNNorth1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.EUCentral1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.EUWest1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.EUWest2, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.SAEast1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.USEast1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.USEast2, null);
-            //await controller.SyncToDynamo(Amazon.RegionEndpoint.USGovCloudWest1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.USWest1, null);
-            await controller.SyncToDynamo(Amazon.RegionEndpoint.USWest2, null);
+            while (true) {
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.APNortheast1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.APNortheast2, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.APSouth1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.APSoutheast1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.APSoutheast2, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.CACentral1, null);
+                //await controller.SyncToDynamo(Amazon.RegionEndpoint.CNNorth1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.EUCentral1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.EUWest1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.EUWest2, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.SAEast1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.USEast1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.USEast2, null);
+                //await controller.SyncToDynamo(Amazon.RegionEndpoint.USGovCloudWest1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.USWest1, null);
+                await controller.SyncToDynamo(Amazon.RegionEndpoint.USWest2, null);
+            }
+
 
 
 
@@ -60,30 +70,71 @@ namespace Functions
         private AmazonCloudWatchClient cloudWatch = new AmazonCloudWatchClient();
 
 
-        [LambdaSerializer(typeof(JsonSerializer))]
-        public async Task SyncToCloudWatch(Amazon.Lambda.DynamoDBEvents.DynamoDBEvent @event, ILambdaContext context) {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task SyncToCloudWatch(DynamoDBEvent @event, ILambdaContext context) {
 
-            Console.Write(.@event);
-        
-            // TODO: figure out if the record has not been recorded in CloudWatch, then record it.
+           
 
-            foreach (var record in @event.Records) {
-                Console.WriteLine(record.EventName);
+           // Console.Write(JsonConvert.SerializeObject(@event));
+           
+            var records = @event.Records.ToList();
+            var tasks = new List<Task>();
 
+            while (records.Count > 0) {
 
-                //record.Dynamodb.NewImage["PartitionKey"]
+                
+                var maxRows = records.Count >= 20 ? 20 : records.Count;
+                var rowsToSync = records.GetRange(0, maxRows);
+                records.RemoveRange(0, maxRows);
 
+                Console.WriteLine("About to write records to cloudwatch: " + rowsToSync);
+                var metricData = new List<MetricDatum>();
+
+                foreach (var record in rowsToSync)
+                {
+                    //Console.WriteLine(record.EventName);
+                    metricData.Add(new MetricDatum
+                    {
+                        MetricName = "PricePerECU",
+                        Dimensions = new List<Dimension>{
+                                 new Dimension{
+                                     Name = "Area",
+                                     Value = record.Dynamodb.NewImage["AvailabilityZone"].S.Split('-')[0]
+                                 },
+                                 new Dimension{
+                                     Name = "Region",
+                                     Value = record.Dynamodb.NewImage["AvailabilityZone"].S.Split('-')[1]
+                                 },
+                                 new Dimension{
+                                     Name = "AZ",
+                                     Value = record.Dynamodb.NewImage["AvailabilityZone"].S.Split('-')[2]
+                                 },
+                                 new Dimension{
+                                     Name = "InstanceFamily",
+                                     Value = record.Dynamodb.NewImage["InstanceType"].S.Split('.')[0]
+                                 },
+                                 new Dimension{
+                                     Name = "InstanceType",
+                                     Value = record.Dynamodb.NewImage["InstanceType"].S.Split('.')[1]
+                                 },
+                                 new Dimension{
+                                     Name = "OS",
+                                     Value = record.Dynamodb.NewImage["ProductDescription"].S
+                                 }
+                            },
+                        Timestamp = DateTime.Parse(record.Dynamodb.NewImage["Timestamp"].S),
+                        Value = Convert.ToDouble(record.Dynamodb.NewImage["PricePerECU"].N)
+                    });
+                }
+
+                tasks.Add(cloudWatch.PutMetricDataAsync(new PutMetricDataRequest
+                {
+                    Namespace = "SpotPriceData2",
+                    MetricData = metricData
+                }));
             }
 
-
-
-            throw new NotImplementedException();
-            /*
-            cloudWatch.PutMetricDataAsync(new Amazon.CloudWatch.Model.PutMetricDataRequest
-            {
-                Namespace = "SpotPriceHistory"
-                //MetricData = new }
-            );*/
+            await Task.WhenAll(tasks);
         }
 
 
@@ -113,11 +164,12 @@ namespace Functions
                    PutRequest = new PutRequest {
                        Item = new Dictionary<string, AttributeValue> {
                            { "PartitionKey", new AttributeValue(string.Format("{0}|{1}|{2}", r.AvailabilityZone, r.InstanceType, r.ProductDescription)) },
-                           {"SortKey", new AttributeValue(Convert.ToString(r.Timestamp)) },
+                           {"SortKey", new AttributeValue(Convert.ToString(r.Timestamp.ToUniversalTime())) },
+                           {"AvailabilityZone", new AttributeValue(r.AvailabilityZone) },
                            {"Price", new AttributeValue(Convert.ToString(r.Price)) },
                            {"ProductDescription", new AttributeValue(r.ProductDescription) },
                            {"InstanceType", new AttributeValue(r.InstanceType) },
-                           {"Timestamp", new AttributeValue(Convert.ToString(r.Timestamp)) },
+                           {"Timestamp", new AttributeValue(Convert.ToString(r.Timestamp.ToUniversalTime())) },
                            { "PricePerCPU", new AttributeValue{
                             N = Convert.ToString(Convert.ToDecimal(r.Price) / instanceTypes.Single(it=>it.Code==r.InstanceType).CPU)
                            }
@@ -141,7 +193,7 @@ namespace Functions
                 });
 
                 // throttle the write rate
-                await Task.WhenAll(writeTask, Task.Delay(500));
+                await Task.WhenAll(writeTask, Task.Delay(100));
                 
             }
         }
