cd5d04fadefcdb8ca367e611c41f4c35a075e683
paulfryer@macrowebservices.com <paulfryer@macrowebservices.com>
2017-06-14 15:37:56 -0700

    more work on emr controller.

--- Functions/EMRController.cs
+++ Functions/EMRController.cs
@@ -4,6 +4,7 @@ using Amazon.Lambda.Core;
 using Amazon.ElasticMapReduce;
 using Amazon.ElasticMapReduce.Model;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Functions
 {
@@ -12,21 +13,21 @@ namespace Functions
         IAmazonElasticMapReduce emr = new AmazonElasticMapReduceClient();
 
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-        public async Task<dynamic> CheckForJobs(ProcessJobState e)
+        public async Task<dynamic> StartCluster(ProcessJobState e)
         {
 
             if (string.IsNullOrEmpty(e.ClusterServiceRole))
                 e.ClusterServiceRole = "EMR_DefaultRole";
             if (string.IsNullOrEmpty(e.ClusterJobFlowRole))
                 e.ClusterJobFlowRole = "EMR_EC2_DefaultRole";
-            
+
             var masterFleet = new InstanceFleetConfig
             {
                 InstanceFleetType = "MASTER",
                 TargetOnDemandCapacity = 1,
                 InstanceTypeConfigs = new List<InstanceTypeConfig>{
                                 new InstanceTypeConfig{
-                                    InstanceType = "m3.xlarge"
+                                    InstanceType = "m1.medium"
                                 }
                             }
             };
@@ -37,7 +38,7 @@ namespace Functions
                 TargetOnDemandCapacity = 1,
                 InstanceTypeConfigs = new List<InstanceTypeConfig>{
                                 new InstanceTypeConfig{
-                                    InstanceType = "m3.xlarge"
+                                    InstanceType = "m1.medium"
                                 }
                             }
             };
@@ -48,7 +49,7 @@ namespace Functions
                 TargetSpotCapacity = 2,
                 InstanceTypeConfigs = new List<InstanceTypeConfig>{
                                 new InstanceTypeConfig{
-                                    InstanceType = "m3.xlarge"
+                                    InstanceType = "m1.medium"
                                 }
                             }
             };
@@ -56,7 +57,7 @@ namespace Functions
             var runResp = await emr.RunJobFlowAsync(
                 new RunJobFlowRequest
                 {
-                    AmiVersion = "emr-5.2.0",
+                    ReleaseLabel = "emr-5.2.0",
                     Name = "AutoProvisionedEmrCluster",
                     ServiceRole = e.ClusterServiceRole,
                     JobFlowRole = e.ClusterJobFlowRole,
@@ -78,14 +79,54 @@ namespace Functions
             return e;
         }
 
-    }
 
-    public class ProcessJobState
-    {
 
-        public string ClusterServiceRole { get; set; }
-        public string ClusterJobFlowRole { get; set; }
-        public string JobFlowId { get; set; }
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<dynamic> GetClusterStatus(ProcessJobState e)
+        {
 
+            var jobFlowsResp = await emr.DescribeJobFlowsAsync(new DescribeJobFlowsRequest
+            {
+                JobFlowIds = new List<string> { e.JobFlowId }
+            });
+
+            e.ClusterStatus = jobFlowsResp.JobFlows.First().ExecutionStatusDetail.State.Value;
+
+            /*
+            var resp = await emr.DescribeClusterAsync(new DescribeClusterRequest{
+                ClusterId = e.JobFlowId
+            });
+
+            e.ClusterStatus = resp.Cluster.Status.State.Value;
+            */
+
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<dynamic> SubmitJob(ProcessJobState e)
+        {
+            // TODO: submit job here.
+            await Task.Delay(1000);
+
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<dynamic> TerminateCluster(ProcessJobState e)
+        {
+            var resp = await emr.TerminateJobFlowsAsync(new TerminateJobFlowsRequest { JobFlowIds = new List<string> { e.JobFlowId } });
+
+            return e;
+        }
+
+        public class ProcessJobState
+        {
+
+            public string ClusterServiceRole { get; set; }
+            public string ClusterJobFlowRole { get; set; }
+            public string JobFlowId { get; set; }
+            public string ClusterStatus { get; set; }
+        }
     }
 }
