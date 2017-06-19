6790c8443a9ffa33fe024bffd9829cd7a75de7f8
Paul Fryer <paulfryer@macrowebservices.com>
2017-06-18 20:32:52 -0700

    goiogle

--- SinglePageApp/js/controllers/prices-PR.js
+++ SinglePageApp/js/controllers/prices-PR.js
@@ -16,7 +16,7 @@ module.exports = function($scope, $location, $route, $routeParams, UtilityServic
         $scope.resp = JSON.parse(this.responseText);
         
         console.log($scope.resp);
-        //loadChart();
+        loadChart();
         $scope.$apply();
     }
   };
@@ -25,6 +25,126 @@ module.exports = function($scope, $location, $route, $routeParams, UtilityServic
 
 
   function loadChart(){
+    $scope.myChartObject = {
+      "type": "LineChart",
+      "displayed": false,
+      "data": {
+        "cols": [
+          {
+            "id": "month",
+            "label": "Month",
+            "type": "string",
+            "p": {}
+          },
+          {
+            "id": "laptop-id",
+            "label": "Laptop",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "desktop-id",
+            "label": "Desktop",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "server-id",
+            "label": "Server",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "cost-id",
+            "label": "Shipping",
+            "type": "number"
+          }
+        ],
+        "rows": [
+          {
+            "c": [
+              {
+                "v": "January"
+              },
+              {
+                "v": 19,
+                "f": "42 items"
+              },
+              {
+                "v": 12,
+                "f": "Ony 12 items"
+              },
+              {
+                "v": 7,
+                "f": "7 servers"
+              },
+              {
+                "v": 4
+              }
+            ]
+          },
+          {
+            "c": [
+              {
+                "v": "February"
+              },
+              {
+                "v": 13
+              },
+              {
+                "v": 1,
+                "f": "1 unit (Out of stock this month)"
+              },
+              {
+                "v": 12
+              },
+              {
+                "v": 2
+              }
+            ]
+          },
+          {
+            "c": [
+              {
+                "v": "March"
+              },
+              {
+                "v": 24
+              },
+              {
+                "v": 5
+              },
+              {
+                "v": 11
+              },
+              {
+                "v": 6
+              }
+            ]
+          }
+        ]
+      },
+      "options": {
+        "title": "Sales per month",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+          "title": "Sales unit",
+          "gridlines": {
+            "count": 10
+          }
+        },
+        "hAxis": {
+          "title": "Date"
+        }
+      },
+      "formatters": {}
+    }
+
+  }
+
+  function loadChart2(){
       $scope.labels = [];
       $scope.series = [];
       $scope.data = [];
