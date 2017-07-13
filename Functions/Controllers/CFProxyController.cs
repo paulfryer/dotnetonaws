79bb8c30ab27d91f4a991f5546b9013f839250f3
Paul Fryer <paulfryer@macrowebservices.com>
2017-07-12 19:38:46 -0700

    starting a step function that builds a cloud front service proxy.

---
+++ Functions/Controllers/CFProxyController.cs
@@ -0,0 +1,71 @@
+﻿using Amazon.Lambda;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.StepFunctions;
+using Amazon.StepFunctions.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public class CFProxyController
+    {
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<CFProxyState> ValidateInputParameters(CFProxyState e)
+        {
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<CFProxyState> RequestCert(CFProxyState e)
+        {
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<CFProxyState> GetCertApprovalStatus(CFProxyState e)
+        {
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<CFProxyState> CreateCloudFrontDistribution(CFProxyState e)
+        {
+            return e;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<CFProxyState> CreateRoute53CNAME(CFProxyState e)
+        {
+            return e;
+        }
+    }
+
+    public class CFProxyState {
+
+        public string WAFARN { get; set; }
+        public string Regions { get; set; }
+        public string Services { get; set; }
+        public string DomainName { get; set; }
+
+        public bool CertExists { get; set; }
+        public bool CertIsApproved { get; set; }
+
+        public int RegionsToProcess { get; set; }
+        public int ServicesToProcess { get; set; }
+
+        public bool DistributionExists { get; set; }
+        public bool CNAMEExists { get; set; }
+
+    }
+
+
+}
